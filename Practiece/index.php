8ed3bf57148d9739dfc2fe2562e95b5a78583bd1
Raktim Biswas <raktimb181@gmail.com>
2024-07-28 20:48:43 +0530

    checkbox

--- Practiece/index.php
+++ Practiece/index.php
@@ -4,46 +4,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>radio button</title>
+    <title>Document</title>
 </head>
 
 <body>
     <form action="index.php" method="post">
-        <input type="radio" id="visa" name="payment" value="visa">
-        <label for="visa">Visa</label>
+        <label for="pizza">Pizza</label>
+        <input type="checkbox" name="pizza" id="pizza">
         <br>
-        <input type="radio" id="mastercard" name="payment" value="mastercard">
-        <label for="mastercard">Mastercard</label>
+        <label for="biriyani">Biriyani</label>
+        <input type="checkbox" name="biriyani" id="biriyani">
         <br>
-        <input type="radio" id="american_express" name="payment" value="american express">
-        <label for="american_express">American Express</label>
+        <label for="roll">Egg chicken roll</label>
+        <input type="checkbox" name="roll" id="roll">
         <br>
-        <input type="submit" name="submit" value="Submit">
+        <input type="submit" value="submit" name="submit">
     </form>
 </body>
 
 </html>
-
-
 <?php
-$payment = $_POST["payment"];
-if (isset($_POST["payment"])) {
-    $payment =$_POST['payment'];
-}
-
-    switch ($payment) {
-        case "visa":
-            echo "Visa is selected";
-            break;
-        case "mastercard":
-            echo "Mastercard is selected";
-            break;
-        case "american express":
-            echo "American express is selected";
-            break;
-        default:
-            echo "you selected nothing";
-            break;
+if (isset($_POST["submit"])) {
+    if (isset($_POST["pizza"])) {
+        echo "order is pizza<br>";
     }
-
+    if (isset($_POST["biriyani"])) {
+        echo "order is biriyani<br>";
+    }
+    if (isset($_POST["roll"]))
+        echo "order is chicken roll<br>";
+    }
+    if (empty($_POST["pizza"])) {
+        echo "order is not pizza<br>";
+    }
+    if (empty($_POST["biriyani"])) {
+        echo "order is not biriyani<br>";
+    }
+    if (empty($_POST["roll"])) {
+        echo "order is not chicken roll<br>";
+    }
 ?>
