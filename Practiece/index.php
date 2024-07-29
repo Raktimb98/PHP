31b33aa432fea50ca62b599e3d3d6dec5751714a
Raktim Biswas <raktimb181@gmail.com>
2024-07-29 15:39:05 +0530

    validation

--- Practiece/index.php
+++ Practiece/index.php
@@ -1,7 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label for="username">Username:</label>
+        <input type="text" name="username" id="username"><br>
+        <label for="age">age:</label>
+        <input type="text" name="age" id="age"><br>
+        <label for="email">email:</label>
+        <input type="text" name="email" id="email"><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+
+</html>
 <?php
-    function hypotenuse($a , $b){
-        $c =sqrt($a ** 2 + $b ** 2);
-        return $c;
+if (isset($_POST["submit"])) {
+    // $username = filter_input($INPUT_POST, "username", FILTER_SANITIZE_SPECIAL_CHARS);
+    $age =filter_input(INPUT_POST,"age", FILTER_VALIDATE_INT);
+    // $email = filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL);
+    // echo "hello {$username}";
+    if(empty($age)){
+        echo "Enter a valid age";
     }
-    echo hypotenuse(12 , 4);
+    else{
+        echo "you are {$age} years old";
+    }
+}
 ?>
