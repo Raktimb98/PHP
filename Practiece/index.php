ded686de6356f3c0d7d496f4361623283daff739
Raktim Biswas <raktimb181@gmail.com>
2024-07-29 14:18:26 +0530

    function for even

--- Practiece/index.php
+++ Practiece/index.php
@@ -1,46 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <form action="index.php" method="post">
-        <label for="pizza">Pizza</label>
-        <input type="checkbox" name="pizza" id="pizza">
-        <br>
-        <label for="biriyani">Biriyani</label>
-        <input type="checkbox" name="biriyani" id="biriyani">
-        <br>
-        <label for="roll">Egg chicken roll</label>
-        <input type="checkbox" name="roll" id="roll">
-        <br>
-        <input type="submit" value="submit" name="submit">
-    </form>
-</body>
-
-</html>
 <?php
-if (isset($_POST["submit"])) {
-    if (isset($_POST["pizza"])) {
-        echo "order is pizza<br>";
+    function iseven($number){
+        if($number % 2 == 0){
+            echo "even";
     }
-    if (isset($_POST["biriyani"])) {
-        echo "order is biriyani<br>";
+    else{
+        echo "odd";
     }
-    if (isset($_POST["roll"]))
-        echo "order is chicken roll<br>";
-    }
-    if (empty($_POST["pizza"])) {
-        echo "order is not pizza<br>";
-    }
-    if (empty($_POST["biriyani"])) {
-        echo "order is not biriyani<br>";
-    }
-    if (empty($_POST["roll"])) {
-        echo "order is not chicken roll<br>";
-    }
+}
+iseven(13);
 ?>
