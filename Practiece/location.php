45e8fd71098639db72af14032869e573e307605d
Raktim Biswas <raktimb181@gmail.com>
2024-07-30 19:42:01 +0530

    include function

--- Practiece/location.php
+++ Practiece/location.php
@@ -6,11 +6,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Document</title>
 </head>
 <body>
-    This is the home page <br>
-    Stuff about your home page can go here <br>
+This is the location page <br>
+Stuff location your home page can go here <br>
 </body>
 </html>
 <?php
