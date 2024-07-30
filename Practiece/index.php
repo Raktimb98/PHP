267197bced3154581c373c0df603371ee84487ba
Raktim Biswas <raktimb181@gmail.com>
2024-07-30 20:03:40 +0530

    Cookie

--- Practiece/index.php
+++ Practiece/index.php
@@ -1,18 +1,15 @@
 <?php
-    include("header.html");
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    This is the home page <br>
-    Stuff about your home page can go here <br>
-</body>
-</html>
-<?php
-    include("footer.html");
+    setcookie("food","Egg chicken roll",time()+(10000),"/");
+    setcookie("drink","milkshake",time()+(10),"/");
+    
+    /*foreach($_COOKIE as $key =>$value){
+        echo"{$key} = {$value} <br>";
+    }
+    */
+    if(isset($_COOKIE["food"])){
+        echo "Buy some {$_COOKIE["food"]}!";
+    }
+    else{
+        echo "I don't know your favorite food";
+    }
 ?>
