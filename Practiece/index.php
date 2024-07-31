2efc0f4cbe9a17f78071812f7af47357ef6059d6
Raktim Biswas <raktimb181@gmail.com>
2024-07-31 16:07:04 +0530

    hashing

--- Practiece/index.php
+++ Practiece/index.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="<?php htmlspecialchars( $_SERVER["PHP_SELF"])?>" method="post">
-        <label for="username">Username</label>
-        <input type="text" name="username" id="username">
-        <input type="submit" value="submit">
-    </form>
-</body>
-</html>
 <?php
-    /*foreach($_SERVER as $key => $value){
-        echo "{$key} = {$value}";
-    }*/
-    
+    $password = "eggchickenroll";
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    echo $hash."<br>";
+    if(password_verify($password,$hash)){
+        echo "you are logged in!<br>";
+    }
+    else{
+        echo "you are not logged in!<br>";
+    }
 ?>
