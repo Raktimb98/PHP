420ab1c2f62cdab76594e39cc339fd3df3bc9aa1
Raktim Biswas <raktimb181@gmail.com>
2024-07-31 15:55:19 +0530

    server

--- Practiece/index.php
+++ Practiece/index.php
@@ -1,34 +1,21 @@
-<?php
-    session_start();
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>session</title>
+    <title>Document</title>
 </head>
 <body>
-<form action="index.php" method="post">
+    <form action="<?php htmlspecialchars( $_SERVER["PHP_SELF"])?>" method="post">
         <label for="username">Username</label>
-        <input type="text" id="username" name="username"><br><br>
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password"><br><br>
-        <input type="submit" value="submit" name="submit">
+        <input type="text" name="username" id="username">
+        <input type="submit" value="submit">
     </form>
 </body>
 </html>
 <?php
-    if(isset($_POST["submit"])){
-        if(!empty($_POST["username"]) && !empty($_POST["password"])){
-            $_SESSION["username"] = $_POST["username"];
-            $_SESSION["password"] = $_POST["password"];
-            // header("Location: sample.php");
-            // echo $_SESSION["username"]."<br>";
-            // echo $_SESSION["password"]."<br>";
-        }
-        else{
-            echo "Please fill in all fields";
-        }
-    }
+    /*foreach($_SERVER as $key => $value){
+        echo "{$key} = {$value}";
+    }*/
+    
 ?>
