9e95147851c2f9179af0a3e90e313d146d0766a0
Raktim Biswas <raktimb181@gmail.com>
2024-07-31 15:40:10 +0530

    session

--- Practiece/index.php
+++ Practiece/index.php
@@ -1,15 +1,34 @@
 <?php
-    setcookie("food","Egg chicken roll",time()+(10000),"/");
-    setcookie("drink","milkshake",time()+(10),"/");
-    
-    /*foreach($_COOKIE as $key =>$value){
-        echo"{$key} = {$value} <br>";
-    }
-    */
-    if(isset($_COOKIE["food"])){
-        echo "Buy some {$_COOKIE["food"]}!";
-    }
-    else{
-        echo "I don't know your favorite food";
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>session</title>
+</head>
+<body>
+<form action="index.php" method="post">
+        <label for="username">Username</label>
+        <input type="text" id="username" name="username"><br><br>
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password"><br><br>
+        <input type="submit" value="submit" name="submit">
+    </form>
+</body>
+</html>
+<?php
+    if(isset($_POST["submit"])){
+        if(!empty($_POST["username"]) && !empty($_POST["password"])){
+            $_SESSION["username"] = $_POST["username"];
+            $_SESSION["password"] = $_POST["password"];
+            // header("Location: sample.php");
+            // echo $_SESSION["username"]."<br>";
+            // echo $_SESSION["password"]."<br>";
+        }
+        else{
+            echo "Please fill in all fields";
+        }
     }
 ?>
--- Practiece/sample.php
+++ Practiece/sample.php
@@ -1,42 +1,18 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>radio button</title>
+    <title>Document</title>
 </head>
-
 <body>
-    <form action="index.php" method="post">
-        <input type="radio" id="visa" name="payment" value="visa">
-        <label for="visa">Visa</label>
-        <br>
-        <input type="radio" id="mastercard" name="payment" value="mastercard">
-        <label for="mastercard">Mastercard</label>
-        <br>
-        <input type="radio" id="american_express" name="payment" value="american express">
-        <label for="american_express">American Express</label>
-        <br>
-        <input type="submit" name="submit" value="Submit">
-    </form>
-
-    <?php
-    $payment = $_POST["payment"];
-    switch ($payment) {
-        case "visa":
-            echo "Visa is selected";
-            break;
-        case "mastercard":
-            echo "Mastercard is selected";
-            break;
-        case "american express":
-            echo "American Express is selected";
-            break;
-        default:
-            echo "You selected nothing";
-    }
-    ?>
+    
 </body>
-
 </html>
+<?php
+    echo $_SESSION["username"]. "<br>";
+    echo $_SESSION["password"]. "<br>";
+?>
