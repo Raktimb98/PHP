962e988fbd01c8019ae8fb6ca6b9098a5b8f02f2
Raktim Biswas <raktimb181@gmail.com>
2025-03-18 12:42:29 +0530

    Get rows, Get single rows, rows count function

--- CrudAdvance/partials/user.php
+++ CrudAdvance/partials/user.php
@@ -29,11 +29,49 @@ class User extends Database{
     }
     // Function to get rows
 
+    public function getRows($start=0, $limit=10){
+        $sql = "SELECT * FROM {$this->tableName} ORDER BY DESC LIMIT {$start}, {$limit}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        if($stmt->rowCount() > 0){
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
+        }
+        else{
+            $result = [];
+            return $result;
+        }
+    }
+
     // Function to get single row
+
+    public function getRow($id){
+        $sql = "SELECT * FROM {$this->tableName} WHERE id=:id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['id'=>$id]);
+        if($stmt->rowCount() > 0){
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $result;
+        }
+        else{
+            $result = [];
+            return $result;
+        }
+    }
 
     //Function to count no of rows
 
+    public function getRowCount(){
+        $sql = "SELECT count(*) as pcount FROM {$this->tableName}";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $result['pcount'];
+    }
+
     // Function to upload photo
+
+    
 
     // Function to update user
 
