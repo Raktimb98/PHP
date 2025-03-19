cdb37abdfcd35e76b4083ee9ff109ebbe522473c
Raktim Biswas <raktimb181@gmail.com>
2025-03-19 15:39:02 +0530

    Upload file function

--- CrudAdvance/partials/user.php
+++ CrudAdvance/partials/user.php
@@ -71,7 +71,24 @@ class User extends Database{
 
     // Function to upload photo
 
-    
+    public function uploadPhoto($file){
+        if(!empty($file)){
+            $fileTempPath = $file['tmp_name'];
+            $fileName = $file['name'];
+            $fileType = $file['type'];
+            $fileNameCmps = explode(".", $fileName);
+            $fileExtension = strtolower(end($fileNameCmps));
+            $newFileName = md5(time().$fileName).'.'.$fileExtension;
+            $allowedExtensions = array('jpg', 'png', 'jpeg');
+            if(in_array($fileExtension, $allowedExtensions)){
+                $uploadFileDir = getcwd().'/uploads/';
+                $destFilePath = $uploadFileDir.$newFileName;
+                if(move_uploaded_file($fileTempPath, $destFilePath)){
+                    return $newFileName;
+                }
+            }
+        }
+    }
 
     // Function to update user
 
